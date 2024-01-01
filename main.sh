422b9666a356b346b0ecbf0582d2ed704ab60f86
veesamsrinivasarao <155358450+veesamsrinivasarao@users.noreply.github.com>
2024-01-01 16:49:09 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a simple shell script
+
+echo "Hello! What is your name?"
+
+# Read user input into the variable 'name'
+read name
+
+# Print a personalized greeting
+echo "Nice to meet you, $name!
